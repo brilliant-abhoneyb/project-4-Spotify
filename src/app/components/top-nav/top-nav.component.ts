d1c6af23bfdd4defeb3bed58bc0b75261384c3e1
Assel <assel.bulekbayeva.work@gmail.com>
2024-11-28 16:01:06 +0500

    continue

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-top-nav',
+  selector: 'app-top-nav',  
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { TopNavComponent } from '../../components/top-nav/top-nav.component';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home',  
+  standalone: true,
+  imports: [ TopNavComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'] 
 })
